f94f31d340092c47e12b891b04340c336acec87a
NguyenPhuLap3112 <43986876+NguyenPhuLap3112@users.noreply.github.com>
2018-10-18 14:40:38 +0700

    xây dựng hàm tăng mã tự động và hoàn thiện bài

---
+++ QuanLiHocSinh/QuanLiHocSinh/autoID.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuanLiHocSinh
+{
+    class autoID
+    {
+        public static string NextID(string lastID, string prefixID)
+        {
+            int nextID = int.Parse(lastID.Remove(0, prefixID.Length)) + 1;
+            int lengthNumberID = lastID.Length - prefixID.Length;
+            string zeroNumber = "";
+            for (int i = 1; i <= lengthNumberID; i++)
+            {
+                if (nextID < Math.Pow(10, i))
+                {
+                    for (int j = 1; j <= lengthNumberID - i; i++)
+                    {
+                        zeroNumber += "0";
+                    }
+                    return prefixID + zeroNumber + nextID.ToString();
+
+                }
+            }
+            return prefixID + nextID;
+        }
+        public string NextID2()
+        {
+            string a = "";
+            return a;
+
+
+        }
+    }
+}
